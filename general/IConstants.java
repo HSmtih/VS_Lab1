46916c14cb8415eec798643c60b4e5648c5a707c
networker <networker@lab28.cpt.haw-hamburg.de>
2014-11-07 13:44:43 +0100

    final server impl

--- general/IConstants.java
+++ general/IConstants.java
@@ -5,8 +5,8 @@ public interface IConstants {
     static final int MAX_QUEUE_LEN = 10;
     
     static final int MAX_RETRIES = 5;
-    static final int RETRY_TIMEOUT = 3000; // time in ms
-    static final int CLIENT_TIMEOUT = 6000; // time in ms
+    static final int RETRY_TIMEOUT = 10000; // time in ms
+    static final int CLIENT_TIMEOUT = 5000; // time in ms
     
     static final String SERVER_NAME = "SERVER";
     static final String LOG_FILE_PATH = "Log.txt";
